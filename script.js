dc36c01fa398c3f892ee569ff3507abb6f4efd4b
Mondli Buthelezi <67345545+MondliButhelezi@users.noreply.github.com>
2024-09-07 19:30:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const menuToggle = document.getElementById('menu-toggle');
 const pinnedNav = document.getElementById('pinned-nav');
 const menuLinks = document.querySelectorAll('.menu-link');
 
+// Toggle the navigation menu
 menuToggle.addEventListener('click', () => {
     pinnedNav.classList.toggle('show');
 });
 
+// Close the navigation when a link is clicked
 menuLinks.forEach(link => {
     link.addEventListener('click', () => {
         pinnedNav.classList.remove('show');
